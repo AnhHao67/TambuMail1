50cb75161dfdc05c73689c43ff998006b554f8dc
Hào Đinh Anh <2033180037@hufi.edu.vn>
2021-10-12 22:50:38 +0700

    Create TambuMailDbContext

---
+++ TambuMail.Data/EF/TambuMailDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TambuMail.Data.EF
+{
+    public class TambuMailDbContext : DbContext
+    {
+
+    }
+}
